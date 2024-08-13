f55837c6430bfe35f257de71abd4df0ba72a4b1e
dwheeler3039 <52417272+dwheeler3039@users.noreply.github.com>
2024-08-13 14:50:36 +0000

    run file and exercis file updates

--- Exercise Files/Chap01/auto.cpp
+++ Exercise Files/Chap01/auto.cpp
@@ -5,6 +5,11 @@
 #include <string>
 using namespace std;
 
+template <typename lT, typename rT>
+auto tf(const lT & lhs, const rT & rhs){
+    return lhs + rhs;
+}
+
 int main() {
     int i = 47;
     const char * cstr = "this is a c-string";
@@ -19,5 +24,19 @@ int main() {
     cout << "type of x is " << typeid(x).name() << endl;
     cout << "type of y is " << typeid(y).name() << endl;
     
+    // for( auto it = sclass.begin(); it != sclass.end(); ++it ){
+    //     cout << *it << " ";
+    // }
+    // cout << endl;
+
+    for( auto c : sclass ) {
+        cout << c << " ";
+    }
+    cout << endl;
+
+    auto z = tf<string, const char *>(sclass, cstr);
+    cout << "z is " << z << endl;
+    cout << "type of z is " << typeid(z).name() << endl;
+
     return 0;
 }
--- Exercise Files/Chap01/template-function.cpp
+++ Exercise Files/Chap01/template-function.cpp
@@ -2,6 +2,7 @@
 // updated 2018-08-18
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 template <typename T>
@@ -9,11 +10,22 @@ T maxof ( const T & a, const T & b ) {
     return ( a > b ? a : b );
 }
 
+template <typename T>
+void printv(const vector<T> & v) {
+    if(v.empty()) return;
+    for(const T & i : v) cout << i << " ";
+    cout << endl;
+}
+
 int main() {
     int a = 7;
     int b = 9;
+
+    vector<int> v1 = { 1, 2, 3, 4, 5 };
     
-    cout << "max is " << maxof<int>( a, b ) << endl;
+    cout << "max is " << maxof( a, b ) << endl;
+
+    printv(v1);
     
     return 0;
 }
